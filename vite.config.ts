174a656b0422278902465280b63fe58440caec87
Benjamin Niccum <necro@Benjamins-MacBook-Air.local>
2025-07-23 19:34:52 -0500

    Use relative paths for better GitHub Pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/ESOBuilder/' : '/',
+  base: './',
   server: {
     port: 3000,
     open: true
   }
-}))
+})
