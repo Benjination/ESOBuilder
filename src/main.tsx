e2932f13c20ba452633fee62092dd2e0d179438a
Benjamin Niccum <necro@Benjamins-MacBook-Air.local>
2025-07-23 19:31:22 -0500

    Fix GitHub Pages routing with HashRouter and SPA redirect script

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
-// Set basename for GitHub Pages deployment
-const basename = window.location.hostname === 'benjination.github.io' 
-  ? '/ESOBuilder' 
-  : '';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
